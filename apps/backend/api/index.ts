9e372846f5152baea003521b68080a617220cdc5
Allam Manikanta <test@Manis-MacBook-Pro.local>
2025-04-30 13:32:41 -0700

    chore: Added env file for local development and Prod build

--- apps/backend/api/index.ts
+++ apps/backend/api/index.ts
@@ -1,12 +1,15 @@
 import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
 import serverless from "serverless-http";
 
 import tasksRouter from "./tasks";
 import usersRouter from "./users";
 
+dotenv.config(); // Load environment variables from .env file
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
@@ -20,27 +23,8 @@ app.use("/api/tasks", tasksRouter);
 export const handler = serverless(app);
 
 // Local development for testing
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV === "development") {
   app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
 }
-
-// import cors from 'cors';
-// import express from 'express';
-
-// import tasksRouter from "./routes/tasks";
-// import usersRouter from './routes/users';
-
-// const app = express();
-// const PORT = 3000;
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/api/users", usersRouter);
-// app.use("/api/tasks", tasksRouter);
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
